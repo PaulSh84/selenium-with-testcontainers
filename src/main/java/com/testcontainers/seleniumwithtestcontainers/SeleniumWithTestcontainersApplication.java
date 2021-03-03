a696b18482677ed084106d0c2e83c7cb4e0aca68
Paul Shirmovski <pshirmovski@gmail.com>
2021-03-03 23:00:59 +0100

    Initial commit

---
+++ src/main/java/com/testcontainers/seleniumwithtestcontainers/SeleniumWithTestcontainersApplication.java
@@ -0,0 +1,13 @@
+package com.testcontainers.seleniumwithtestcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumWithTestcontainersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeleniumWithTestcontainersApplication.class, args);
+    }
+
+}
